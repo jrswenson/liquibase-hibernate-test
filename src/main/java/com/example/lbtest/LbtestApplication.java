1f02561df14f3557e9090c0439eef676cfb1c30f
Jeff Swenson <jeff.swenson.ext@sodexo.com>
2021-04-08 13:58:23 -0600

    Initial

---
+++ src/main/java/com/example/lbtest/LbtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.lbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lbtest/LbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
